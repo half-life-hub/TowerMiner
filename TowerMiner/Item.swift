5c07a68eaf1b09471eb158ea429f941910b122e8
Steven Marshall <pinkfloydim@me.com>
2026-05-07 11:41:46 +0930

    Initial Commit

---
+++ TowerMiner/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  TowerMiner
+//
+//  Created by Steven Marshall on 7/5/2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ TowerMinerTests/TowerMinerTests.swift
@@ -0,0 +1,19 @@
+//
+//  TowerMinerTests.swift
+//  TowerMinerTests
+//
+//  Created by Steven Marshall on 7/5/2026.
+//
+
+import Testing
+@testable import TowerMiner
+
+struct TowerMinerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
